b9047df640cf9a9e56bd33c98f90a19f12a9773e
Mukund Agrawal <mukundagrawal1234@gmail.com>
2021-03-03 15:14:14 +0530

    spring boot thymeleaf heroku example

---
+++ src/main/java/com/mukund/JavaGuideHerokuDeploySpringbootmvcWebThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.mukund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGuideHerokuDeploySpringbootmvcWebThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGuideHerokuDeploySpringbootmvcWebThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mukund/JavaGuideHerokuDeploySpringbootmvcWebThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mukund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGuideHerokuDeploySpringbootmvcWebThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
